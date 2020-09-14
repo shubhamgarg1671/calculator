888883f4de8955077f71eea85993c5d0d277f0c9
unknown <shubhamgarg1671@gmail.com>
2020-09-14 16:25:52 +0530

    second commit

--- server.js
+++ server.js
@@ -2,13 +2,15 @@ var express = require("express");
 
 var app = express();
 
+var port = process.env.PORT || 8080;
+
 app.use(express.static('public'));
 
 //make way for some custom css, js and images
 app.use('/css', express.static(__dirname + '/public/css'));
 app.use('/js', express.static(__dirname + '/public/js'));
 
-var server = app.listen(8081, function(){
+var server = app.listen(port, function(){
     var port = server.address().port;
     console.log("Server started at http://localhost:%s", port);
 });
